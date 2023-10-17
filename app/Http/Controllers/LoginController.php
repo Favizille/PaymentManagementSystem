f575c1f751c898623882638e79628e3cd5307348
unknown <favourgabriel50@gmail.com>
2023-10-18 00:59:18 +0100

    added login function to user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,16 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Repository\loginRepository;
 
 class LoginController extends Controller
 {
-    public function login(){
+    public function __construct(loginRepository $loginRepository)
+    {
+        $this->loginRepository = $loginRepository;
+    }
+
+    public function login()
+    {
         return view("signIn");
     }
 
-    public function logginIn(LoginRequest $login)
+    public function logginIn(LoginRequest $request)
     {
+        if(! $this->loginRepository->logIn($request->validated()) )
+        {
+            return false;
+        }
 
+        return view("catalog");
     }
 }
